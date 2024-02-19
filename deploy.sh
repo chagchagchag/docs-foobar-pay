943e2661e000ae29360fb726711a649342e3f582
chagchagchag <chagchagchag.dev@gmail.com>
2024-02-19 10:33:33 +0900

    first status

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+echo ">>> rm -rf build"
+rm -rf build
+
+echo ""
+echo ">>> rm -rf .next"
+rm -rf .next
+
+echo ""
+echo ">>> rm -rf out"
+rm -rf out
+
+echo ""
+echo ">>> yarn deploy"
+yarn deploy
